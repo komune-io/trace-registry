862e3ce92417db5645fc16e1f195776f2a2094a1
Adrien Poupard <adrien@smartb.city>
2023-07-26 11:39:59 +0200

    refactor(cccev-init): remove unused code and update access token handling for better security feat(cccev-init): add support for new access token structure to improve security refactor(ProjectFakeBuilder.kt): update createRandomProject function to accept AccessToken object instead of string for better type safety refactor(init.kt): update main function to use new access token structure and remove unused code for cleaner codebase

--- platform/cccev/cccev-init/src/main/kotlin/city/smartb/registry/program/cccev/init.kt
+++ platform/cccev/cccev-init/src/main/kotlin/city/smartb/registry/program/cccev/init.kt
@@ -6,70 +6,36 @@ import city.smartb.registry.program.cccev.initRequirement
 import kotlinx.coroutines.runBlocking
 
 fun main() = runBlocking {
-//    Prod
-    val urlAuth ="https://auth.connect.smartb.network/realms/sb"
-//    val client = System.getenv("PROD_TRACE_CLIENT_ID")!!
-//    val secret = System.getenv("PROD_TRACE_CLIENT_SECRET")!!
-    val urlVer = "https://trace.smartb.network/ver"
-//    val urlCCCEV = "https://trace.smartb.network/cccev"
-
-//    PreProd
-//    val urlAuth = "https://auth.connect.smart-b.io/realms/sb-dev"
-//    val clientSb = "tr-smart-b-app"
-//    val secretSb = "49225b01-2de5-4507-90f2-74e639274646"
-//    val urlCCCEV = "https://trace.smart-b.io/cccev"
-//    val urlVer = "https://trace.smart-b.io/ver"
-
 //    Local
-//    val urlAuth = "https://auth.connect.smart-b.io/realms/sb-dev"
-//    val urlCCCEV = "http://localhost:8083"
-//    val urlVer = "http://localhost:8070"
-//    val clientSb = "sb-plateform-app"
-//    val secretSb = "secretsecret"
+    val urlAuth = "https://auth.dev.connect.smart-b.io/realms/sb-dev"
+    val urlCCCEV = "http://localhost:8083"
+    val urlVer = "http://localhost:8070"
 
-//    val nameIssuer = "Rockfeller"
-//    val clientIssuer = "sb-rockfeller-app"
-//    val secretIssuer = "IY4wICIbvqHtVQFuvkrYvhQ4j5sEydwV"
-
-//    val nameIssuer = "Smartb"
-//    val clientIssuer = "tr-smart-b-app"
-//    val secretIssuer = "49225b01-2de5-4507-90f2-74e639274646"
-
-//    val nameOffseter = "Phease"
-//    val clientOffseter = "sb-phease-app"
-//    val secretOffseter = "ZcX92th9ca4FYdZ2wwUnnzi8btPCZDZH"
-
-//    val nameOffseter = "CleanChain"
-//    val clientOffseter = "sb-cleanchain-app"
-//    val secretOffseter = "EH1VG8BjsTe7h0OrPumeDPJUXh9xdA5l"
+    val nameProjectManager = "Smartb"
+    val clientProjectManager = "tr-smartb-ver2-app"
+    val secretProjectManager = "30ef2c5b-acee-460b-9f38-2ecd9855792d"
 
     val nameIssuer = "Smartb"
-    val clientIssuer = "tr-smartb-asset-pool-app"
-    val secretIssuer = "40419323-22eb-40a3-9dc6-b7feceeccff0"
+    val clientIssuer = "tr-smartb-ver2-app"
+    val secretIssuer = "30ef2c5b-acee-460b-9f38-2ecd9855792d"
 
     val nameOffseter = "CleanChain"
-    val clientOffseter = "tr-cleanchain-asset-app"
-    val secretOffseter = "9c26689b-ff78-40e5-acbd-2ab0cbaeb41e"
+    val clientOffseter = "tr-smartb-ver2-app"
+    val secretOffseter = "30ef2c5b-acee-460b-9f38-2ecd9855792d"
 
 
-//    tr-smartb-asset-pool-app
-//    40419323-22eb-40a3-9dc6-b7feceeccff0
-
-//    tr-cleanchain-asset-app
-//    9c26689b-ff78-40e5-acbd-2ab0cbaeb41e
-
+    val accessTokenProjectManager = AppAuth.getActor(urlAuth, nameProjectManager, clientProjectManager, secretProjectManager)
     val accessTokenIssuer = AppAuth.getActor(urlAuth, nameIssuer, clientIssuer, secretIssuer)
     val accessTokenOffseter = AppAuth.getActor(urlAuth, nameOffseter, clientOffseter, secretOffseter)
 
-//    initRequirement(urlCCCEV)
-//    initIndicatorsCarbon(urlCCCEV)
-//    createRandomProject(urlVer, accessTokenSb)
+    initRequirement(urlCCCEV)
+    initIndicatorsCarbon(urlCCCEV)
 
+//    val assetPoolId = null
     val assetPoolId = createAssetPool(
         urlVer,
         issuer = accessTokenIssuer,
         offsetter = accessTokenOffseter
     )
-//    createRandomProject(urlVer, accessTokenSb)
-//    createBrazilRockFeller(urlVer, accessTokenSb, accessTokenRockfeller, accessTokenOffseter)
+    createRandomProject(urlVer, accessTokenProjectManager.accessToken, assetPoolId)
 }
