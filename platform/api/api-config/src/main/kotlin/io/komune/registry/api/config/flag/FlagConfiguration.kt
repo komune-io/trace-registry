4f8b5983434fd12450413431f6d1b99eb9026d78
Adrien <email = 1469248+apoupard@users.noreply.github.com>
2025-01-15 17:16:32 +0100

    feat(flag): add FlagConfiguration and FlagProperties for feature toggling feat(application.yml): configure feature flags for control, data, project, and identity modules The new FlagConfiguration class provides a mechanism for routing functions based on feature flags, allowing for dynamic enabling or disabling of specific functionalities. The FlagProperties class holds the configuration for these flags, which are now defined in the application.yml file, enabling easier management of feature toggles in the application.

---
+++ platform/api/api-config/src/main/kotlin/io/komune/registry/api/config/flag/FlagConfiguration.kt
@@ -0,0 +1,93 @@
+package io.komune.registry.api.config.flag
+
+import f2.dsl.fnc.f2Function
+import java.net.URI
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.BeanFactory
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.cloud.function.context.FunctionCatalog
+import org.springframework.cloud.function.context.FunctionProperties
+import org.springframework.cloud.function.context.MessageRoutingCallback
+import org.springframework.cloud.function.context.config.RoutingFunction
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.expression.BeanFactoryResolver
+import org.springframework.messaging.Message
+
+@EnableConfigurationProperties(FlagProperties::class)
+@Configuration(proxyBeanMethods = false)
+class FlagConfiguration(private val flagProperties: FlagProperties) {
+
+    @Bean
+    fun customRouter(): MessageRoutingCallback {
+        return FlagMessageRoutingCallback(flagProperties)
+    }
+
+    @Bean
+    fun project(functionCatalog: FunctionCatalog, functionProperties: FunctionProperties, beanFactory: BeanFactory, callback: MessageRoutingCallback): RoutingFunction {
+        return RoutingFunction(functionCatalog, functionProperties, BeanFactoryResolver(beanFactory), callback)
+    }
+
+    @Bean
+    fun control(functionCatalog: FunctionCatalog, functionProperties: FunctionProperties, beanFactory: BeanFactory, callback: MessageRoutingCallback): RoutingFunction {
+        return RoutingFunction(functionCatalog, functionProperties, BeanFactoryResolver(beanFactory), callback)
+    }
+
+    @Bean
+    fun data(functionCatalog: FunctionCatalog, functionProperties: FunctionProperties, beanFactory: BeanFactory, callback: MessageRoutingCallback): RoutingFunction {
+        return RoutingFunction(functionCatalog, functionProperties, BeanFactoryResolver(beanFactory), callback)
+    }
+
+    @Bean
+    fun identity(functionCatalog: FunctionCatalog, functionProperties: FunctionProperties, beanFactory: BeanFactory, callback: MessageRoutingCallback): RoutingFunction {
+        return RoutingFunction(functionCatalog, functionProperties, BeanFactoryResolver(beanFactory), callback)
+    }
+
+    @Bean
+    fun disabledFunction(flagProperties: FlagProperties) = f2Function<String, Boolean> {
+        throw IllegalStateException("Function is disabled")
+    }
+}
+
+class FlagMessageRoutingCallback(private val flagProperties: FlagProperties):  MessageRoutingCallback {
+    private val logger = LoggerFactory.getLogger(javaClass)
+
+    override fun routingResult(message: Message<*>): String {
+        val uri = message.headers["uri"] as? String ?: return "disabledFunction"
+        val path = URI(uri).path // "cataloguePage"
+        val pathParts = path.split("/").filter { it.isNotEmpty() }
+        val module = pathParts[pathParts.size - 2]  // "catalogue"
+        val functionName = pathParts.last()
+
+        // Define routing logic as a map for better readability
+        val modulePrefixFlagMap = mapOf(
+            "control" to mapOf(
+                "activity" to flagProperties.module.control,
+                "dcs" to flagProperties.module.control,
+            ),
+            "data" to mapOf(
+                "catalogue" to flagProperties.module.data,
+                "dataset" to flagProperties.module.data,
+            ),
+            "identity" to mapOf(
+                "user" to flagProperties.module.identity,
+            ),
+            "project" to mapOf(
+                "chat" to flagProperties.module.project,
+                "project" to flagProperties.module.project,
+                "asset" to flagProperties.module.project
+            ),
+        )
+
+        val isEnable =  modulePrefixFlagMap[module]?.any { (prefix, functionFlag) ->
+            functionName.startsWith(prefix) && functionFlag
+        } ?: true
+
+        if (!isEnable) {
+            logger.info("Routing to disabled function: $module")
+            return "disabledFunction"
+        }
+
+        return functionName
+    }
+}
