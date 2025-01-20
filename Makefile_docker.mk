9f4d6a35c51899a90d520acaf25676b1fc76cfaa
Adrien <email = 1469248+apoupard@users.noreply.github.com>
2025-01-20 15:56:47 +0100

    feat(docker): add Keycloak Docker support and configuration fix(package.json): correct spelling of registry in Keycloak package name The changes introduce a new Keycloak Docker setup, including a Dockerfile and Makefile targets for building, publishing, and promoting the Keycloak image. Additionally, the spelling of "registry" in the Keycloak package name is corrected to ensure consistency and accuracy. This enhances the deployment process and improves the overall structure of the project.

--- Makefile_docker.mk
+++ Makefile_docker.mk
@@ -1,4 +1,5 @@
 VERSION = $(shell cat VERSION)
+include .env_version
 DOCKER_REPOSITORY = ghcr.io/
 
 GATEWAY_NAME	   	:= ${DOCKER_REPOSITORY}komune-io/trace-registry-gateway
@@ -24,6 +25,9 @@ POSTGRES_NAME	    	:= trace-registry-postgres
 POSTGRES_IMG	    	:= ${POSTGRES_NAME}:${VERSION}
 POSTGRES_LATEST			:= ${POSTGRES_NAME}:latest
 
+KEYCLOAK_DOCKERFILE	    := infra/docker/keycloak/Dockerfile
+KEYCLOAK_NAME	   	 	:= trace-registry-keycloak
+KEYCLOAK_IMG	    	:= ${KEYCLOAK_NAME}:${VERSION}
 
 .PHONY: lint build test publish promote
 
@@ -98,3 +102,22 @@ docker-docker-promote:
 		--build-arg VERSION=${VERSION} \
 		-f ${POSTGRES_DOCKERFILE} \
 		-t docker.io/komune/${POSTGRES_IMG} .
+
+## keycloak
+docker-keycloak-lint:
+	@docker run --rm -i hadolint/hadolint hadolint - < ${KEYCLOAK_DOCKERFILE}
+
+docker-keycloak-build:
+	@docker build --no-cache=true \
+		--progress=plain \
+		--build-arg VERSION_CONNECT=${VERSION_CONNECT} \
+		--build-arg VERSION_NODE=${VERSION_NODE} \
+		-f ${KEYCLOAK_DOCKERFILE} -t ${KEYCLOAK_IMG} .
+
+docker-keycloak-publish:
+	@docker tag ${KEYCLOAK_IMG} ghcr.io/komune-io/${KEYCLOAK_IMG}
+	@docker push ghcr.io/komune-io/${KEYCLOAK_IMG}
+
+docker-keycloak-promote:
+	@docker tag ${KEYCLOAK_IMG} docker.io/komune/${KEYCLOAK_IMG}
+	@docker push docker.io/komune/${KEYCLOAK_IMG}
