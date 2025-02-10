ba7a6c25fdd5d5257bd7049a726b1f33342ff8da
Adrien <email = 1469248+apoupard@users.noreply.github.com>
2025-02-10 18:44:37 +0100

    chore(Makefile_libs.mk): remove unnecessary sudo commands for modifying /etc/hosts chore(dev-compose.mk): add debug echo statements to improve visibility during the build process Removing the sudo commands for modifying /etc/hosts simplifies the test-pre step, making it less error-prone and more user-friendly. Adding debug echo statements in the dev-envsubst target enhances visibility into the build process, helping developers understand what actions are being performed and aiding in troubleshooting.

--- Makefile_libs.mk
+++ Makefile_libs.mk
@@ -9,9 +9,6 @@ build:
 	VERSION=${VERSION} ./gradlew build --stacktrace -x test -x jvmTest -x allTests -x jsBrowserTest
 
 test-pre:
-	sudo echo "127.0.0.1 ca.bc-coop.bclan" | sudo tee -a /etc/hosts
-	sudo echo "127.0.0.1 peer0.bc-coop.bclan" | sudo tee -a /etc/hosts
-	sudo echo "127.0.0.1 orderer.bclan" | sudo tee -a /etc/hosts
 	@make dev pull
 	@make dev up
 	@sleep 10
