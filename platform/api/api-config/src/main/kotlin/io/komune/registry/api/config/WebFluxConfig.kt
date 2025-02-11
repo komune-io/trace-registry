e9a5495e3519ec699c8272d3a68da07e968a1360
Luc Heydel <luc@kosm.io>
2025-02-11 16:16:08 +0100

    configure commands deserialization to ignore unknown keys and to allow implicit nulls

---
+++ platform/api/api-config/src/main/kotlin/io/komune/registry/api/config/WebFluxConfig.kt
@@ -0,0 +1,38 @@
+package io.komune.registry.api.config
+
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
+import org.springframework.http.codec.ServerCodecConfigurer
+import org.springframework.http.codec.json.KotlinSerializationJsonDecoder
+import org.springframework.http.codec.json.KotlinSerializationJsonEncoder
+import org.springframework.web.reactive.config.WebFluxConfigurer
+
+@OptIn(ExperimentalSerializationApi::class)
+@Configuration
+class WebFluxConfig : WebFluxConfigurer {
+
+    @Bean
+    @Primary
+    fun json(): Json = Json {
+        ignoreUnknownKeys = true
+        explicitNulls = false
+    }
+
+    @Bean
+    fun kotlinSerializationJsonDecoder(json: Json): KotlinSerializationJsonDecoder {
+        return KotlinSerializationJsonDecoder(json)
+    }
+
+    @Bean
+    fun kotlinSerializationJsonEncoder(json: Json): KotlinSerializationJsonEncoder {
+        return KotlinSerializationJsonEncoder(json)
+    }
+
+    override fun configureHttpMessageCodecs(configurer: ServerCodecConfigurer) {
+        configurer.customCodecs().register(kotlinSerializationJsonDecoder(json()))
+        configurer.customCodecs().register(kotlinSerializationJsonEncoder(json()))
+    }
+}
