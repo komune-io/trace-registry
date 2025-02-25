ec33f0982128a1dd2ce092b1091124eb8ad12baf
Adrien <email = 1469248+apoupard@users.noreply.github.com>
2025-02-25 14:51:19 +0100

    feat(theme): add theme configuration to support customizable colors in the application refactor(theme): update theme usage to pull colors from the new config structure The changes introduce a theme configuration that allows for customizable colors in the application. This enhances the flexibility of the UI by enabling different color schemes. The theme-related code is refactored to utilize the new configuration structure, improving maintainability and consistency across the application.

--- platform/web/packages/domain/src/config/index.ts
+++ platform/web/packages/domain/src/config/index.ts
@@ -5,7 +5,19 @@ export interface PlatformConfig {
 
 export interface Config {
     platform: PlatformConfig
+    theme?: ThemeConfig
 }
+
+export interface ThemeConfig {
+    colors?: ThemeColorsConfig,
+}
+
+export interface ThemeColorsConfig {
+    primary?: string,
+    secondary?: string,
+    background?: string
+}
+
 
 // @ts-ignore
 export const config: () => Config = () => window._env_.config
