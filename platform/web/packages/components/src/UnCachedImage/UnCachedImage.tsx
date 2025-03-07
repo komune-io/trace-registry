12d95db8a652653835e93ac06a0a01ddc366ec6e
basile-savouret <47100280+basile-savouret@users.noreply.github.com>
2025-03-07 10:31:34 +0100

    forwardRef on UnCachedImage

--- platform/web/packages/components/src/UnCachedImage/UnCachedImage.tsx
+++ platform/web/packages/components/src/UnCachedImage/UnCachedImage.tsx
@@ -1,12 +1,13 @@
-import React, { useState } from 'react'
+import React, { ForwardedRef, useState } from 'react'
 
-export const UnCachedImage = (props: React.ComponentPropsWithRef<"img">) => {
+export const UnCachedImage = React.forwardRef((props: React.ComponentPropsWithRef<"img">, ref: ForwardedRef<HTMLImageElement>) => {
     const { src, ...other } = props
     const [timeStamp] = useState(new Date().getTime())
     return (
         <img
             src={src + "?t=" + timeStamp}
             {...other}
+            ref={ref}
         />
     )
-}
+})
