f73efd3369f30d6d4476fffffb98c5b6705e9fe6
Adrien <email = 1469248+apoupard@users.noreply.github.com>
2025-04-02 01:32:14 +0200

    Init new certificate

--- platform/infra/pdf/src/test/kotlin/io/komune/registry/infra/pdf/CertificateGeneratorTest.kt
+++ platform/infra/pdf/src/test/kotlin/io/komune/registry/infra/pdf/CertificateGeneratorTest.kt
@@ -10,7 +10,16 @@ class CertificateGeneratorTest {
     fun fill() {
 //        val certifiedBy = "Sustainable Future Group"
 //        val project = "Certicongo"
-        val result = CertificateGenerator.fillPendingCertificate("lala", 1002020, "lala", BigDecimal.TEN, "lala")
+        val result = CertificateGenerator.fillPendingCertificate(
+            transactionId = "lala",
+            date = 1002020,
+            issuedTo = "Orange",
+            quantity = BigDecimal.TEN,
+            indicator = "tonnes de CO2 économisées",
+            certifiedBy = "Kerdos",
+            project = "Installation de panneaux potovoltaiques sur bâtiments tertiaires"
+
+        )
         val pdf = File("build/certificate.pdf")
         pdf.writeBytes(result)
         println(pdf.writeBytes(result))
