cae1bef3588f8e9de9f70f4e48a7f183bb5ec290
Adrien <email = 1469248+apoupard@users.noreply.github.com>
2025-04-09 23:29:26 +0200

    fix: add null check for catalogue in useCatalogueMenu and log item details

--- platform/web/packages/web-app/src/App/menu/useCatalogueMenu.tsx
+++ platform/web/packages/web-app/src/App/menu/useCatalogueMenu.tsx
@@ -12,6 +12,9 @@ function asMenu(item: CatalogueRefTree, cataloguesAll: CatalogueAll, location: L
     return item.catalogues?.flatMap( (it) => asMenu(it, cataloguesAll, location)) ?? []
   }
   const catalogue = isAlias(item) ? item.catalogues?.[0]! : item
+  console.log("item", item)
+  console.log("catalogue", catalogue)
+  if(!catalogue) return []
   const catalogueLink = isBranch(catalogue)
     ? cataloguesAll(catalogue?.identifier!)
     : cataloguesAll(item?.identifier!, catalogue?.identifier!)
