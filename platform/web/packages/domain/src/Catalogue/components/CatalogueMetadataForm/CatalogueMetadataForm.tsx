fa1e2335a73daea3dcad7d8c10d290ea445b1db5
basile-savouret <47100280+basile-savouret@users.noreply.github.com>
2025-04-18 11:38:28 +0200

    fiox themes field for solution

--- platform/web/packages/domain/src/Catalogue/components/CatalogueMetadataForm/CatalogueMetadataForm.tsx
+++ platform/web/packages/domain/src/Catalogue/components/CatalogueMetadataForm/CatalogueMetadataForm.tsx
@@ -181,6 +181,7 @@ export const CatalogueMetadataForm = (props: CatalogueMetadataFormProps) => {
             const relatedCataloguesToIds = convertRelatedCataloguesToIds(relatedCatalogues)
             const toSave = {
                 ...others,
+                themes: [values.themes],
                 relatedCatalogueIds: relatedCataloguesToIds,
             }
             if (onSubmit) {
