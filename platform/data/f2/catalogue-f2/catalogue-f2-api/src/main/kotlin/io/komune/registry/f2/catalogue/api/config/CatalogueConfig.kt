c13ec6dd91fc2688e98c4f61a20fa8814274c74a
Luc Heydel <luc@kosm.io>
2025-04-18 10:03:31 +0200

    make catalogue/dataset type config files path configurable

--- platform/data/f2/catalogue-f2/catalogue-f2-api/src/main/kotlin/io/komune/registry/f2/catalogue/api/config/CatalogueConfig.kt
+++ platform/data/f2/catalogue-f2/catalogue-f2-api/src/main/kotlin/io/komune/registry/f2/catalogue/api/config/CatalogueConfig.kt
@@ -1,29 +1,30 @@
 package io.komune.registry.f2.catalogue.api.config
 
 import io.komune.registry.api.commons.utils.jsonMapper
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver
 import s2.spring.utils.logger.Logger
 
 @Configuration
 class CatalogueConfig {
-
-    companion object {
-        const val TEMPLATE_DIR = "classpath:catalogues"
-    }
-
     private val logger by Logger()
 
-    final val typeConfigurations: Map<String, CatalogueTypeConfiguration> = PathMatchingResourcePatternResolver()
-        .getResources("$TEMPLATE_DIR/*.json")
-        .associate { file ->
-            val typeConfiguration = try {
-                jsonMapper.readValue(file.inputStream, CatalogueTypeConfiguration::class.java)
-            } catch (e: Exception) {
-                logger.error("Error while parsing file ${file.url}", e)
-                throw e
-            }
+    @Value("\${platform.catalogue.type-files}")
+    private lateinit var typeConfigDir: String
 
-            typeConfiguration.type to typeConfiguration
-        }
+    final val typeConfigurations: Map<String, CatalogueTypeConfiguration> by lazy {
+        PathMatchingResourcePatternResolver()
+            .getResources("$typeConfigDir/*.json")
+            .associate { file ->
+                val typeConfiguration = try {
+                    jsonMapper.readValue(file.inputStream, CatalogueTypeConfiguration::class.java)
+                } catch (e: Exception) {
+                    logger.error("Error while parsing file ${file.url}", e)
+                    throw e
+                }
+
+                typeConfiguration.type to typeConfiguration
+            }.also { println(it) }
+    }
 }
