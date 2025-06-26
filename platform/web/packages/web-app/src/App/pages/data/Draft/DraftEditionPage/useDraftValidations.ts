860d39e4d66a1fb6ddb723e8fab614ca3b6a6b0f
basile-savouret <47100280+basile-savouret@users.noreply.github.com>
2025-06-26 15:41:35 +0200

    add refetch draft on validation

---
+++ platform/web/packages/web-app/src/App/pages/data/Draft/DraftEditionPage/useDraftValidations.ts
@@ -0,0 +1,89 @@
+import {CatalogueDraft, useCatalogueDraftSubmitCommand, useCatalogueDraftValidateCommand} from 'domain-components'
+import {useCallback} from 'react'
+import {useNavigate} from 'react-router-dom'
+import {useQueryClient} from '@tanstack/react-query';
+import {useRoutesDefinition} from 'components'
+import {FormComposableState} from '@komune-io/g2';
+
+interface useDraftValidationsParams {
+  draft?: CatalogueDraft
+  refetchDraft: () => void
+  afterValidateNavigate?: string
+  metadataFormState: FormComposableState
+  setTab: (tab: string) => void
+}
+
+export const useDraftValidations = (params: useDraftValidationsParams) => {
+  const { draft, refetchDraft, afterValidateNavigate, metadataFormState, setTab } = params
+  const queryClient = useQueryClient()
+  const navigate = useNavigate()
+  const { cataloguesContributions } = useRoutesDefinition()
+
+  const validateDraft = useCatalogueDraftValidateCommand({})
+
+  const validateMetadata = useCallback(
+    async () => {
+      const errors = await metadataFormState.validateForm()
+      if (Object.keys(errors).length > 0) {
+        setTab("metadata")
+        return false
+      }
+      return true
+    },
+    [metadataFormState.validateForm, setTab],
+  )
+
+  const onValidate = useCallback(
+    async () => {
+      if (!draft || !(await validateMetadata())) return
+
+      const res = await validateDraft.mutateAsync({
+        id: draft.id,
+      })
+      if (res) {
+        queryClient.invalidateQueries({ queryKey: ["data/catalogueGet", { id: draft.catalogue.id }] })
+        queryClient.invalidateQueries({ queryKey: ["data/catalogueGetByIdentifier", { identifier: draft.originalCatalogueId }] })
+        queryClient.invalidateQueries({ queryKey: ["data/catalogueDraftGet", { id: draft.id }] })
+        queryClient.invalidateQueries({ queryKey: ["data/catalogueDraftPage"] })
+        queryClient.invalidateQueries({ queryKey: ["data/cataloguePage"] })
+        queryClient.invalidateQueries({ queryKey: ["data/catalogueRefGetTree"] })
+        queryClient.invalidateQueries({ queryKey: ["data/catalogueListAvailableParents"] })
+
+        if (draft.catalogue.parent) {
+            queryClient.invalidateQueries({ queryKey: ["data/catalogueGet", { id: draft.catalogue.parent.id }] })
+            queryClient.invalidateQueries({ queryKey: ["data/catalogueGetByIdentifier", { identifier: draft.catalogue.parent.identifier }] })
+        }
+
+        navigate(afterValidateNavigate ?? cataloguesContributions())
+      }
+    },
+    [draft, afterValidateNavigate, queryClient.invalidateQueries, validateMetadata, metadataFormState],
+  )
+
+  const submitDraft = useCatalogueDraftSubmitCommand({})
+
+  const onSubmit = useCallback(
+    async (reason: string) => {
+      if (!draft) return
+      const res = await submitDraft.mutateAsync({
+        id: draft.id,
+        versionNotes: reason
+      })
+      if (res) {
+        refetchDraft()
+        queryClient.invalidateQueries({ queryKey: ["data/catalogueGet", { id: draft.catalogue.id }] })
+        queryClient.invalidateQueries({ queryKey: ["data/catalogueGetByIdentifier", { id: draft.catalogue.identifier }] })
+        queryClient.invalidateQueries({ queryKey: ["data/catalogueDraftPage"] })
+        navigate(cataloguesContributions() + "?successfullContribution=true")
+      }
+    },
+    [draft, queryClient.invalidateQueries],
+
+  )
+
+  return {
+    onValidate,
+    onSubmit,
+    validateMetadata
+  }
+}
