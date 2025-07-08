d822046feb2a5805036272edff9d33439638c9f1
basile-savouret <47100280+basile-savouret@users.noreply.github.com>
2025-07-08 11:20:03 +0200

    nouvelle fiche et revendiquer when disconnected

--- platform/web/packages/web-app/src/App/menu/CreateCatalogueButton/CreateCatalogueButton.tsx
+++ platform/web/packages/web-app/src/App/menu/CreateCatalogueButton/CreateCatalogueButton.tsx
@@ -1,6 +1,6 @@
 import { AddCircleOutlineRounded } from '@mui/icons-material'
 import { CustomButton, TMSMenuItem, useButtonMenu, useExtendedAuth, useRoutesDefinition } from 'components'
-import {  useCatalogueRefGetTreeQuery } from 'domain-components'
+import { useCatalogueRefGetTreeQuery } from 'domain-components'
 import { useMemo } from 'react'
 import { useTranslation } from 'react-i18next'
 import { BackEndIcon } from '../useCatalogueMenu'
@@ -11,31 +11,43 @@ interface CreateCatalogueButtonProps {
 
 export const CreateCatalogueButton = (props: CreateCatalogueButtonProps) => {
     const { identifier } = props
-    const { policies } = useExtendedAuth()
+    const { policies, keycloak } = useExtendedAuth()
     const { cataloguesCreateType } = useRoutesDefinition()
 
     const { t, i18n } = useTranslation()
 
     const createButtonStruture = useCatalogueRefGetTreeQuery({
         query: {
-          identifier: identifier,
-          language: i18n.language
+            identifier: identifier,
+            language: i18n.language
         }
-      }).data?.item?.structure?.createButton
+    }).data?.item?.structure?.createButton
 
     const items = useMemo(() => createButtonStruture?.types?.map((type): TMSMenuItem => {
         return {
-                key: type.identifier,
-                label: type.name,
-                icon: <BackEndIcon url={type.icon}/>,
-                to: cataloguesCreateType(type.identifier)
-            }
+            key: type.identifier,
+            label: type.name,
+            icon: <BackEndIcon url={type.icon} />,
+            to: cataloguesCreateType(type.identifier)
+        }
     }), [t, createButtonStruture])
 
     const { buttonProps, menu } = useButtonMenu({
         items
     })
 
+    if (!keycloak.isAuthenticated) {
+        return (<CustomButton
+            sx={{
+                width: "100%"
+            }}
+            startIcon={<AddCircleOutlineRounded />}
+            onClick={() => keycloak.login()}
+        >
+            {t("newSheet")}
+        </CustomButton>
+        )
+    }
     if (!createButtonStruture || !policies.catalogue.canCreate()) return null
     return (
         <>
